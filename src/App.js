a8f0c5e0e9cd053056792b37cccc50c0c708d112
daynawhite <dayna26.2@gmail.com>
2024-02-28 20:37:35 -0600

    created userCard and part of app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useState, useEffect } from 'react';
+import UserCard from './components/UserCard';
+
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUsersFromApi = () => {
+    axios.get('https://randomuser.me/api?results=25')
+    // I blindly copied this from the teacher's example but I don't understand it and could not replicate it on my own!
+    .then(res => {
+      const { data } = res;
+      // this.setState({
+      //   users: data.results.map(user => ({...user, show: false}))
+      // })
+      setUsers(data.results.map(user => ({ ...user, show: false }) ))
+    })
+
+  useEffect(getUsersFromApi, [])
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
---
+++ src/components/UserCard.js
@@ -0,0 +1,19 @@
+import { Card } from 'react-bootstrap';
+
+function UserCard(props) {
+    const { user } = props;
+
+    return (
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{user.name.first} {user.name.last}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    )
+}
